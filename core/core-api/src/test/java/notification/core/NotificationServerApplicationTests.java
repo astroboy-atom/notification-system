4c483f311ed2caf48dc97bb5e2d176e0ad2607ff
Haneul Lee <leehaneul990623@gmail.com>
2026-04-09 15:22:01 +0900

    chore: 프로젝트 초기 구조 설정

---
+++ core/core-api/src/test/java/notification/core/NotificationServerApplicationTests.java
@@ -0,0 +1,12 @@
+package notification.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
