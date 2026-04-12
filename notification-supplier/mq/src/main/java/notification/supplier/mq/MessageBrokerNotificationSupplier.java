59d15d71eff6264a59c1ad8cae93b0304dccb55d
Haneul Lee <leehaneul990623@gmail.com>
2026-04-12 21:46:23 +0900

    feat: MessageBrokerNotificationSupplier 구현

---
+++ notification-supplier/mq/src/main/java/notification/supplier/mq/MessageBrokerNotificationSupplier.java
@@ -0,0 +1,20 @@
+package notification.supplier.mq;
+
+import lombok.extern.slf4j.Slf4j;
+import notificaiton.supplier.NotificationSupplier;
+import notification.storage.db.NotificationEntity;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MessageBrokerNotificationSupplier implements NotificationSupplier {
+
+    @Override
+    public void doSend(NotificationEntity notificationEntity) {
+        log.info(
+                "메시지 브로커로 메시지를 전송합니다. notification id = {} topic = {}",
+                notificationEntity.getId(),
+                notificationEntity.getNotificationChanel().name()
+        );
+    }
+}
