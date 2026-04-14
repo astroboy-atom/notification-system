0c2196901b8a09f79cbda8b2a33c3035e783fc1c
Haneul Lee <leehaneul990623@gmail.com>
2026-04-14 21:16:18 +0900

    chore: docker 설정 및 깃 캐시 초기화

---
+++ docker/mysql/init.sql
@@ -0,0 +1,25 @@
+INSERT INTO member_entity (email, in_app_token, is_agree_in_app_push, is_agree_email)
+VALUES
+    ('dummy1@notification.local', 'dummy-in-app-token-1', true, true),
+    ('dummy2@notification.local', 'dummy-in-app-token-2', true, true),
+    ('dummy3@notification.local', 'dummy-in-app-token-3', true, true),
+    ('dummy4@notification.local', 'dummy-in-app-token-4', true, true),
+    ('dummy5@notification.local', 'dummy-in-app-token-5', true, true),
+    ('dummy6@notification.local', 'dummy-in-app-token-6', true, true),
+    ('dummy7@notification.local', 'dummy-in-app-token-7', true, true),
+    ('dummy8@notification.local', 'dummy-in-app-token-8', true, true),
+    ('dummy9@notification.local', 'dummy-in-app-token-9', true, true),
+    ('dummy10@notification.local', 'dummy-in-app-token-10', true, true);
+
+INSERT INTO event_entity (contents)
+VALUES
+    ('더미 이벤트 1'),
+    ('더미 이벤트 2'),
+    ('더미 이벤트 3'),
+    ('더미 이벤트 4'),
+    ('더미 이벤트 5'),
+    ('더미 이벤트 6'),
+    ('더미 이벤트 7'),
+    ('더미 이벤트 8'),
+    ('더미 이벤트 9'),
+    ('더미 이벤트 10');
