0f737391d5740c92468f2c238f0be52ed757281c
Haneul Lee <leehaneul990623@gmail.com>
2026-04-14 12:53:52 +0900

    feat: 모니터링 모듈 구현

---
+++ support/monitoring/src/main/java/notification/monitoring/NotificationMetrics.java
@@ -0,0 +1,23 @@
+package notification.monitoring;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import notification.enums.NotificationChanel;
+import notification.enums.NotificationType;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationMetrics {
+
+    private final MeterRegistry meterRegistry;
+
+    public void incrementFinalFailure(NotificationChanel notificationChanel, NotificationType notificationType) {
+        Counter.builder("notification.final_failure.total")
+                .tag("channel", notificationChanel.name())
+                .tag("type", notificationType.name())
+                .register(meterRegistry)
+                .increment();
+    }
+}
